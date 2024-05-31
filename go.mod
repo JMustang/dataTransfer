99bc226c440c9b24408457d9a1cc6d096116b4a7
Junior <eecfredes@gmail.com>
2024-05-30 21:20:51 -0300

    :tada: Inicio

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JMustang/dataTransfer
+
+go 1.22.3
---
+++ p2p/transport.go
@@ -0,0 +1,7 @@
+package p2p
+
+// Peer é uma interface que representa o nó remoto.
+
+// Transporte é tudo que cuida da comunicação
+// entre os nós da rede. Isto pode ser do
+//formulário (TCP, UDP, websockets, ...)
