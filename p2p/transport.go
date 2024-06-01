efbf65d4d05a7734d1582cd181c0219f54fc2fa0
Junior <eecfredes@gmail.com>
2024-05-31 22:51:06 -0300

    :rocket: transport, tcp_transport, tcp_transport_test

--- p2p/transport.go
+++ p2p/transport.go
@@ -1,7 +1,13 @@
 package p2p
 
 // Peer é uma interface que representa o nó remoto.
+type Peer interface {
+}
 
 // Transporte é tudo que cuida da comunicação
 // entre os nós da rede. Isto pode ser do
-//formulário (TCP, UDP, websockets, ...)
+// formulário (TCP, UDP, websockets, ...)
+type Transporte interface {
+}
+
+// Network é a rede de nós.
