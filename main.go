556306f8b6b4dcaa07b3e8643024efdb8d3ac94c
Junior <eecfredes@gmail.com>
2024-07-18 21:50:58 -0300

    :rocket: Add the tcp transport options

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 )
 
 func main() {
+	tcpOpts := p2p.TCPTransportOpts{
+		ListenAddr:    ":3000",
+		HandshakeFunc: p2p.NOPHandshakefunc,
+	}
 	tr := p2p.NewTCPTransport(":3000")
 	if err := tr.ListenAndAccept(); err != nil {
 		log.Fatal(err)
